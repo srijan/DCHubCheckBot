45feea82e6cc25fd9d8b87c7dae91a05de9e1fe5
Srijan Choudhary <srijan4@gmail.com>
2012-02-13 05:49:28 +0530

    Added checking of all IPs, and adding those found to database.

--- hublist.py
+++ hublist.py
@@ -5,7 +5,10 @@ from botclass import *
 from func import *
 import socket
 import select
+import redis
+from netaddr import IPNetwork
 
+rc = redis.StrictRedis()
 socket.setdefaulttimeout(0.02)
 
 class HubCheckBot(PyBot):
@@ -19,13 +22,15 @@ class HubCheckBot(PyBot):
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def initBot(self,host,port):
-        self.HOST = host
+        self.HOST = str(host)
         self.PORT = port
+        self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def check(self):
         try:
             self.serversocket.connect((self.HOST,self.PORT))
-        except Exception:
+        except Exception,e:
+            self.serversocket.close()
             return False
         ready = select.select([self.serversocket],[],[],1)
         if ready[0]:
@@ -39,6 +44,7 @@ class HubCheckBot(PyBot):
                         if hubmsg[0] == '$Hello':
                             self.serversocket.close()
                             return True
+        self.serversocket.close()
         return False
 
 
@@ -47,4 +53,30 @@ def checkHub(host,port):
     hcb.initBot(host,port)
     return hcb.check()
 
-print checkHub("172.16.12.39",411)
+
+subnetList = [
+                "172.16.1.0/24",    "172.16.2.0/24",    "172.16.3.0/24",
+                "172.16.4.0/24",    "172.16.5.0/24",    "172.16.6.0/24",
+                "172.16.7.0/24",    "172.16.8.0/24",    "172.16.9.0/24",
+                "172.16.10.0/24",   "172.16.11.0/24",   "172.16.12.0/24",
+                "172.16.13.0/24",   "172.16.14.0/24",   "172.16.15.0/24",
+                "172.16.16.0/24",   "172.16.17.0/24",   "172.16.18.0/24",
+                "172.16.19.0/24",   "172.16.20.0/24",   "172.17.1.0/24",
+                "172.17.2.0/24",    "172.17.3.0/24",    "172.17.4.0/24",
+                "172.17.5.0/24",    "172.17.6.0/24",    "172.17.7.0/24",
+                "172.17.8.0/24",    "172.17.9.0/24",    "172.17.10.0/24",
+                "172.17.11.0/24",   "172.17.12.0/24",   "172.17.13.0/24"
+             ]
+
+def addToDb(ip):
+    print "Adding ",ip
+    rc.sadd("hublist",ip)
+
+def generateMainList():
+    for subnet in subnetList:
+        for ip in IPNetwork(subnet).iter_hosts():
+            if checkHub(ip,411):
+                addToDb(ip)
+
+#print checkHub("172.16.12.39",411)
+generateMainList()
