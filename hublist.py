2bf8df47d405608013617c65426d8cc77735491f
Srijan Choudhary <srijan4@gmail.com>
2012-02-13 06:47:17 +0530

    Now hub status of those saved in the db is checked..

--- hublist.py
+++ hublist.py
@@ -7,6 +7,7 @@ import socket
 import select
 import redis
 from netaddr import IPNetwork
+import string
 
 rc = redis.StrictRedis()
 socket.setdefaulttimeout(0.02)
@@ -18,8 +19,11 @@ class HubCheckBot(PyBot):
     botif = "eth0"
     botip = "172.16.3.117"
     debug = 1
+    sharesize = '1501200000000'
     ownernick = "n0w0nd3r"
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    nicklist = []
+    oplist = []
 
     def initBot(self,host,port):
         self.HOST = str(host)
@@ -47,6 +51,46 @@ class HubCheckBot(PyBot):
         self.serversocket.close()
         return False
 
+    def getHubDetails(self):
+        hub = dict({'active': True, 'name': "", 'users': 0, 'ops': 0})
+        try:
+            self.serversocket.connect((self.HOST,self.PORT))
+        except Exception,e:
+            self.serversocket.close()
+            hub['active'] = False
+            return hub
+        ready = select.select([self.serversocket],[],[],1)
+        if ready[0]:
+            while True:
+                t = readsock(self.serversocket)
+                #print t
+                if t != '':
+                    if t[0] == '$':
+                        hubmsg = t.split()
+                        if hubmsg[0] == '$Lock':
+                            self.serversocket.send('$Key '+lock2key2(hubmsg[1]) + '|' + '$ValidateNick ' + self.botnick + '|')
+                        if hubmsg[0] == '$HubName':
+                            hub['name'] = string.join(hubmsg[1:])
+                        if hubmsg[0] == '$Hello':
+                            self.serversocket.close()
+                            return hub
+                            #self.serversocket.send('$Version 0.785|$MyINFO $ALL '+self.botnick+'$ $100$bot@bot.com$'+str(self.sharesize)+'|$GetNickList|')
+                        """
+                        if hubmsg[0]=='$NickList':
+                            nicklist = hubmsg[1].split("$$")
+                            hub['users'] = len(nicklist)
+                        if hubmsg[0]=='$OpList':
+                            oplist = hubmsg[1].split("$$")
+                            hub['ops'] = len(oplist)
+                            self.serversocket.close()
+                            return hub
+                        if hubmsg[0]=='$Search':
+                            self.serversocket.close()
+                            return hub
+                        """
+        self.serversocket.close()
+        hub['active'] = False
+        return hub
 
 def checkHub(host,port):
     hcb = HubCheckBot()
@@ -78,5 +122,19 @@ def generateMainList():
             if checkHub(ip,411):
                 addToDb(ip)
 
+def checkMainList():
+    h = HubCheckBot()
+    for ip in rc.smembers("hublist"):
+        h.initBot(ip,411)
+        hub = h.getHubDetails()
+        print ip,"\t:\t",hub['name'],
+        if hub['active']:
+            print " (Online)"
+        else:
+            print " (Offline)"
+
+
+checkMainList()
+
 #print checkHub("172.16.12.39",411)
-generateMainList()
+#generateMainList()
